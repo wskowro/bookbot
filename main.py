3224962d4a27e76ed75c9c84c425e272e3af06ba
wskowro <wskowro@gmail.com>
2025-04-22 22:47:35 -0400

    Added cmdline argument functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,7 +7,12 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    filepath = './books/frankenstein.txt'
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+
+    filepath = sys.argv[1]
     book = get_book_text(filepath)
     word_count = len(get_word_count(book))
     char_count = get_char_count(book)
